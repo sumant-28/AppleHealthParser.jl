2aef519d2e3d5d463118289a2c898cb29f0c33c6
Documenter.jl <documenter@juliadocs.github.io>
2024-10-01 11:22:59 +0000

    build based on fc514e1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AppleHealthParser","category":"page"},{"location":"#AppleHealthParser","page":"Home","title":"AppleHealthParser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AppleHealthParser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AppleHealthParser]","category":"page"}]
+}
